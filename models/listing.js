42beb5b0e76c1309538573a7c50ea13a65d372d4
Alokdubey1710 <alok17101710k@gmail.com>
2024-09-24 17:38:03 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default:
+            "https://unsplash.com/photos/the-sun-is-shining-through-the-trees-in-the-forest--sidLcag5lo",
+        set: (v) => 
+            v === "" 
+        ? "https://unsplash.com/photos/the-sun-is-shining-through-the-trees-in-the-forest--sidLcag5lo" 
+        : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+});
+
+listingSchema.post("/findOneAndDelete", async (listing)=>{
+    if(listing) {
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
+
+    });
+    
+
+
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
